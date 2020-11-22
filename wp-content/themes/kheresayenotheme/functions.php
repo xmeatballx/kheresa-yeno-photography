b5a86f3c906bf2988013b882c56e485f87fe5d1e
xmeatballx <erik.jensen5@pcc.edu>
2020-11-21 19:00:06 -0800

    added page urls

--- wp-content/themes/kheresayenotheme/functions.php
+++ wp-content/themes/kheresayenotheme/functions.php
@@ -4,5 +4,5 @@
 <tr><th align='left' bgcolor='#f57900' colspan="5"><span style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Error: Call to undefined function add_action() in /Users/fluffy/Local Sites/kheresa-yeno-photography/app/public/wp-content/themes/kheresayenotheme/functions.php on line <i>13</i></th></tr>
 <tr><th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th></tr>
 <tr><th align='center' bgcolor='#eeeeec'>#</th><th align='left' bgcolor='#eeeeec'>Time</th><th align='left' bgcolor='#eeeeec'>Memory</th><th align='left' bgcolor='#eeeeec'>Function</th><th align='left' bgcolor='#eeeeec'>Location</th></tr>
-<tr><td bgcolor='#eeeeec' align='center'>1</td><td bgcolor='#eeeeec' align='center'>0.0003</td><td bgcolor='#eeeeec' align='right'>405056</td><td bgcolor='#eeeeec'>{main}(  )</td><td title='/Users/fluffy/Local Sites/kheresa-yeno-photography/app/public/wp-content/themes/kheresayenotheme/functions.php' bgcolor='#eeeeec'>.../functions.php<b>:</b>0</td></tr>
+<tr><td bgcolor='#eeeeec' align='center'>1</td><td bgcolor='#eeeeec' align='center'>0.0004</td><td bgcolor='#eeeeec' align='right'>405056</td><td bgcolor='#eeeeec'>{main}(  )</td><td title='/Users/fluffy/Local Sites/kheresa-yeno-photography/app/public/wp-content/themes/kheresayenotheme/functions.php' bgcolor='#eeeeec'>.../functions.php<b>:</b>0</td></tr>
 </table></font>
